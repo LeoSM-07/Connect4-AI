c33ab7f614f47f6100bba5a7031d05b8ef308bd9
LeoSM_07 <leosmindlin@icloud.com>
2023-01-03 09:31:38 -0800

    Initial Commit

---
+++ Connect4-Ai/main.c
@@ -0,0 +1,14 @@
+//
+// main.c
+// Connect4-Ai
+//
+// Created by LeoSM_07 on 1/3/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
