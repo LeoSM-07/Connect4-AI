3743c2d654d3acef57e99cb69f6871bdda9544c9
LeoSM_07 <69401930+LeoSM-07@users.noreply.github.com>
2023-01-09 21:27:43 -0800

    Update Functions

--- Connect4-Ai/main.c
+++ Connect4-Ai/main.c
@@ -8,26 +8,31 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-void printBoard(int board[][7]) {
-    printf("\nCurrent Board:\n\n");
+#define width 7
+#define height 6
+void placePiece(int player, int column, int board[][7]);
+void populateBoard(long n, int board[][7]);
+void printBoard(long n);
 
-    int i,j;
-    for(i=5; i>=0; i--) {
-        printf("%d ", i);
-        for (j=0; j<7; j++) {
-            if (board[i][j] == 0) {
-                printf("[ ] ");
-            } else {
-                printf("[%d] ", board[i][j]);
-            }
+int main () {
+    long currentBoard = 23;
 
-        }
-        printf("\n");
-    }
-    printf("   0   1   2   3   4   5   6\n");
+    currentBoard = 44455554221;
+    printBoard(currentBoard);
+    return 0;
 }
 
+bool canPlay(int col) {
+    return true;
+};
+
+/// Add a piece in a give column. Will add to the first empty spot starting from the bottom.
+/// - Parameters:
+///   - player: The player who the piece to be added belongs to
+///   - column: The column to add the piece
+///   - board: The board to add the piece on
 void placePiece(int player, int column, int board[][7]) {
     int boardColumn[6];
 
@@ -44,7 +49,11 @@ void placePiece(int player, int column, int board[][7]) {
     }
 }
 
-void populateBoard(int n, int board[][7]) {
+/// Fill out the board based on a specific `int` instruction set
+/// - Parameters:
+///   - n: The state of the game, each turn represented by a digit of the int
+///   - board: The game board
+void populateBoard(long n, int board[][7]) {
     int i = 0;
     int digits[42];
     while (n > 0) {
@@ -57,17 +66,40 @@ void populateBoard(int n, int board[][7]) {
     for (int j = i - 1; j >= 0; j--) {
         int player = j%2+1;
         int column = digits[j];
-        printf("player %d went in: %d\n", player, column);
+        /* uncomment for verbose printing */
+        // printf("player %d went in: %d\n", player, column);
         placePiece(player, column, board);
     }
 }
 
-int main () {
+/// Nicely prints out the board as it is passed
+void printBoard(long n) {
     int board[6][7] = {0};
+    populateBoard(n, board);
+    printf("\nCurrent Board (%ld):\n\n", n);
 
-    populateBoard(4453, board);
-    printBoard(board);
-    return 0;
+    int i,j;
+    for(i=5; i>=0; i--) {
+        printf("%d ", i);
+        for (j=0; j<7; j++) {
+            int current = board[i][j];
+
+            switch (current) {
+            case 0:
+                printf("[ ] ");
+                break;
+            case 1:
+                printf("[X] ");
+                break;
+            case 2:
+                printf("[O] ");
+                break;
+            }
+
+        }
+        printf("\n");
+    }
+    printf("   0   1   2   3   4   5   6\n");
 }
 
 
